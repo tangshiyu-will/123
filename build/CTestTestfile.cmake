413990cfaa044cc5029618b02655d20143ebcb76
tangshiyu-will <224152@student.uwa.edu.au>
2020-04-22 00:21:46 -0700

    Update Readme Files(testing)

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/tang/catkin_ws_new/src
+# Build directory: /home/tang/catkin_ws_new/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(global_planner)
---
+++ build/global_planner/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/tang/catkin_ws_new/src/global_planner
+# Build directory: /home/tang/catkin_ws_new/build/global_planner
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
